fd773dab290d9552a17b33af5e208c241b5f38bc
Asher T. Scott <ashertlalka@gmail.com>
2024-01-29 21:00:36 -0500

    Solution complete and tested

--- Q5.1/inserter/java/inserter.java
+++ Q5.1/inserter/java/inserter.java
@@ -1,31 +1,38 @@
 
 public class inserter {
-    //Given N and M (32 bit) and bit potsitions i and j
+    //Given N and M (32 bit) and bit positions i and j
     //overwrite M into N such that it starts at bit j+1 and ends at bit i+1
     //assume M can always fit within j and i
     //ex: N=1111111111, M=000, i=2, j=4, result = 1111100011
 
-    public static void insert(int N, int M, int i, int j){
+    public static int insert(int N, int M, int i, int j){
         //shift M up by i
         //clear N from i through j
-        //  create N2 such that it is 0 from i to j and AND it to N
-        //      to create N2, right shift N by i, then left shift by i, thus clearing the left. Re-add from j to the end.
+        //  create a mask that is all 1's except 0's i through j and and it to N
+        //      take an int of all 1's and left shift it by j to get the necessary 0's
+        //      create the 1's we need on the right by creating 1000...x and subbing 1 to get 0111...(x-1)
+        //      add the left and right to get the mask
         //add this new M to N and return
 
-        if (!(i > j && i > 0 && j < 32)) {
+        if (!(i < j && i > 0 && j < 32)) {
             throw new RuntimeException("Failed input validation.");
         }
 
-        System.out.println(Integer.toBinaryString(N));
-        
-
+        int M2 = M << i;
+        int mask = ~0;
+        int Rmask = mask << j;
+        int Lmask = (1 << i) - 1;
+        mask = Rmask | Lmask;
+        int N2 = N & mask;
+        return N2 | M2;
     }
 
     public static void main (String[] args){
-        int N = Integer.parseInt("11111", 2);
-        int M = Integer.parseInt("00000", 2);
+        int N = Integer.parseInt("100000000000000000", 2);
+        int M = Integer.parseInt("10011", 2);
         int i = 2;
-        int j = 1;
-        insert(N, M, i, j);
+        int j = 6;
+        int solution = insert(N, M, i, j);
+        System.out.println(Integer.toBinaryString(solution));
     }
 }
